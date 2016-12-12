fae4d889d57ef8f087b4e2ce3e27fbb785977ffb
nehabandal <npbandal@usfca.edu>
2016-12-12 07:51:50 -0800

    expedia link + google maps pin

--- project/src/cs601/travelHelper/HotelDetailServlet.java
+++ project/src/cs601/travelHelper/HotelDetailServlet.java
@@ -10,9 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,8 +18,12 @@ import java.util.List;
  * Created by npbandal on 11/15/16.
  */
 public class HotelDetailServlet extends BaseServlet {
-    public static final String REVIEW_QUERY_SQL = "select reviewTitle,review,username,rating,date from reviewData where hotelId= ? order by date";
-    public static final String ATTRACTION_QUERY_SQL = "select attractionName,rating,address from attractionData where hotelId= ? order by rating";
+    private String FETCH_HOTEL_SQL =
+            "select hotelData.hotelId,hotelData.hotelName,hotelData.address,hotelData.city, hotelData.state, hotelData.country, avg(rating) as avgRating " +
+                    "from hotelData " +
+                    "LEFT JOIN reviewData on hotelData.hotelId=reviewData.hotelId " +
+                    "where hotelData.hotelId = ?";
+
     private DatabaseConnector db;
 
     public HotelDetailServlet() {
@@ -43,24 +45,42 @@ public class HotelDetailServlet extends BaseServlet {
         int hotelId = Integer.parseInt(request.getParameter("hotelId"));
         session.setAttribute("hotelId", hotelId);
 
-        String hotelName = request.getParameter("hotelName");
-        //session.setAttribute("hotelName", hotelName);
-
         prepareResponse("review",response);
         VelocityEngine ve = (VelocityEngine) request.getServletContext().getAttribute("templateEngine");
         VelocityContext vc = new VelocityContext();
         Template template = ve.getTemplate("web/templates/hotel_detail.vm");
-        PrintWriter out=null;
-        try {
-            out=response.getWriter();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        context.put("hotelName", hotelName);
-        //context.put("reviews", getReviewRows(hotelId));
-        //context.put("attractions", getAttractionRows(hotelId));
-
+        getHotelInfo(hotelId, context);
+        context.put("expediaUrl", "https://www.expedia.com/h" + hotelId + ".hotel-information");
         finishResponse(response);
         return template;
+    }
+
+    private void getHotelInfo(int hotelId, Context context) {
+        Connection connection = null;
+        try {
+            connection = db.getConnection();
+            PreparedStatement statement = connection.prepareStatement(FETCH_HOTEL_SQL);
+            statement.setString(1, String.valueOf(hotelId));
+            ResultSet rs = statement.executeQuery();
+
+            while (rs.next()) {
+                context.put("hotelName", rs.getString("hotelName"));
+                context.put("address", rs.getString("address"));
+                context.put("city", rs.getString("city"));
+                context.put("state", rs.getString("state"));
+                context.put("country", rs.getString("country"));
+                context.put("avgRating", rs.getDouble("avgRating"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
